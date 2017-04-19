22e2e80c5f81384c33bee14a4ff7c9c7747ae9aa
aishu7 <aishjelly@gmail.com>
2017-04-19 17:24:11 +0530

    adding files with indendation

--- mat_diag_add/test_has_arr.pl
+++ mat_diag_add/test_has_arr.pl
@@ -1,43 +1,37 @@
 use warnings;
 use diagnostics;
 use strict;
-chomp(my $num=<STDIN>);
-my @mat=();
-my $diag1=0;
-my $diag2=0;
-foreach my $row(0 .. $num-1) 
-{
-	foreach my $col(0 .. $num-1)
-{
-	 chomp($mat[$row][$col]=<STDIN>);
-}
+chomp( my $num = <STDIN> );
+my @mat   = ();
+my $diag1 = 0;
+my $diag2 = 0;
+foreach my $row ( 0 .. $num - 1 ) {
+    foreach my $col ( 0 .. $num - 1 ) {
+        chomp( $mat[$row][$col] = <STDIN> );
+    }
 }
 print "The matrix is\n";
-foreach my $row(0 .. $num-1)
-{
-	foreach my $col(0 .. $num-1)
-{
-	print  $mat[$row][$col];
-}
-print "\n";
+foreach my $row ( 0 .. $num - 1 ) {
+    foreach my $col ( 0 .. $num - 1 ) {
+        print $mat[$row][$col];
+    }
+    print "\n";
 }
 print "Diagonal1:     ";
 
-foreach my $row(0 .. $num-1)
-{
-	
-	print "$mat[$row][$row] + ";
-	
-$diag1=$diag1+$mat[$row][$row];
+foreach my $row ( 0 .. $num - 1 ) {
+
+    print "$mat[$row][$row] + ";
+
+    $diag1 = $diag1 + $mat[$row][$row];
 
 }
 print " = $diag1\n";
 
 print "Diagonal2:     ";
-foreach my $row(0 .. $num-1)
-{
-	print "$mat[$row][$num-$row-1] + ";
-$diag2=$diag2+$mat[$row][$num-$row-1];
+foreach my $row ( 0 .. $num - 1 ) {
+    print "$mat[$row][$num-$row-1] + ";
+    $diag2 = $diag2 + $mat[$row][ $num - $row - 1 ];
 
 }
 print " = $diag2\n";
--- mat_diag_add/test_matr.pl
+++ mat_diag_add/test_matr.pl
@@ -1,51 +1,45 @@
 use warnings;
 use diagnostics;
 use strict;
-chomp(my $num=<STDIN>);
+chomp( my $num = <STDIN> );
 
-my @mat=();
-my @mat1=();
+my @mat  = ();
+my @mat1 = ();
 
-my $diag1=0;
-my $diag2=0;
-my $var=0;
-foreach my $row(0 .. $num-1) 
-{
-	$var=<STDIN>;
-	@mat=split(' ',$var);
-	push(@mat1, [@mat]);
-	#@var=split($var);
-	#print $var;
+my $diag1 = 0;
+my $diag2 = 0;
+my $var   = 0;
+foreach my $row ( 0 .. $num - 1 ) {
+    $var = <STDIN>;
+    @mat = split( ' ', $var );
+    push( @mat1, [@mat] );
+
+    #@var=split($var);
+    #print $var;
 }
-	
-	
 
-foreach my $row(0 .. $num-1) 
-{
-	foreach my $col(0 .. $num-1)
-{
-	print $mat1[$row][$col];
-}
-print "\n";
+foreach my $row ( 0 .. $num - 1 ) {
+    foreach my $col ( 0 .. $num - 1 ) {
+        print $mat1[$row][$col];
+    }
+    print "\n";
 }
 
 print "Diagonal1:     ";
 
-foreach my $row(0 .. $num-1)
-{
-	
-	print "$mat1[$row][$row] + ";
-	
-$diag1=$diag1+$mat1[$row][$row];
+foreach my $row ( 0 .. $num - 1 ) {
+
+    print "$mat1[$row][$row] + ";
+
+    $diag1 = $diag1 + $mat1[$row][$row];
 
 }
 print " = $diag1\n";
 
 print "Diagonal2:     ";
-foreach my $row(0 .. $num-1)
-{
-	print "$mat1[$row][$num-$row-1] + ";
-$diag2=$diag2+$mat1[$row][$num-$row-1];
+foreach my $row ( 0 .. $num - 1 ) {
+    print "$mat1[$row][$num-$row-1] + ";
+    $diag2 = $diag2 + $mat1[$row][ $num - $row - 1 ];
 
 }
 print " = $diag2\n";
