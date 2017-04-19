fbd4a4eb31acd152fbff83ddf92c141ab98e00b7
aishu7 <aishjelly@gmail.com>
2017-04-19 17:18:49 +0530

    adding files with indendation using perl tidy

--- constructor_references/test_inser.pl
+++ constructor_references/test_inser.pl
@@ -1,9 +1,9 @@
 use Users;
 
-my $new_user=new Users("aishu","1234",1);
-print ("UserName");
+my $new_user = new Users( "aishu", "1234", 1 );
+print("UserName");
 print $new_user->getName;
-print ("Password");
+print("Password");
 print $new_user->getpass;
-print ("Id");
+print("Id");
 print $new_user->getid;
